05871da9ce6c37c63188e86cad2caddf90dcda48
Ashkan Azadmajd <azadmajd@live.com>
2022-01-17 12:21:31 +0330

    first Commit

--- src/main/java/com/example/springopenshift/WriteOnDisk.java
+++ src/main/java/com/example/springopenshift/WriteOnDisk.java
@@ -10,11 +10,13 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.UUID;
 
 @Configuration
 @EnableScheduling
 public class WriteOnDisk {
 
+    UUID uuid = UUID.randomUUID();
 
     @Value("${datasource1.addr}")
     String prefixPath1;
@@ -27,7 +29,7 @@ public class WriteOnDisk {
     @Scheduled(fixedDelay = 500)
     public void whenWriteStringUsingBufferedWritter_thenCorrect()
             throws IOException {
-        String str = "Hello, time is"+new Date();
+        String str = "Hello, time is"+new Date()+uuid.toString();
         String fileName1=prefixPath1+"/store1.db";
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName1));
         writer.write(str);
@@ -37,7 +39,7 @@ public class WriteOnDisk {
     @Scheduled(fixedDelay = 500)
     public void whenWriteStringUsingBufferedWritter_thenCorrect2()
             throws IOException {
-        String str = "Hello, time is"+new Date();
+        String str = "Hello, time is"+new Date()+uuid.toString();
         String fileName2=prefixPath2+"/store2.db";
         BufferedWriter writer2 = new BufferedWriter(new FileWriter(fileName2));
 
